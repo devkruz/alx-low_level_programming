d64a30b6340278c08752efe758e782a77d1f0ba8
Tony_dev <tonyexplorer202@gmail.com>
2022-09-08 12:55:16 -0700

    Add prints exactly
    "Programming is like building a multilingual puzzle" script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Prints a string using puts
+ * Description: This will print the
+ * value of $toPrint to standard
+ * output
+ * Return: 0
+ */
+int main(void)
+{
+	char *toPrint =
+	"\"Programming is like building a multilingual puzzle";
+	puts(toPrint);
+	return (0);
+}
