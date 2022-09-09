524fe5e0bdbd13a085da0acbe2d9e793ecce1f1b
Tony_dev <tonyexplorer202@gmail.com>
2022-09-08 21:30:03 -0700

    Add prints exactly "with proper grammar, but the outcome is a piece of art,"

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Prints a string using printf
+ * Description: This will print the
+ * value of $toPrint to standard
+ * output
+ * Return: 0
+ */
+int main(void)
+{
+	char *toPrint =
+	"with proper grammar, but the outcome is a piece of art,";
+	printf("%s\n", toPrint);
+	return (0);
+}
