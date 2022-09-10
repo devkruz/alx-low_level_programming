7a60d3f6cab5d3eb4f22e3f6d55995d3a52a5207
Tony_dev <tonyexplorer202@gmail.com>
2022-09-09 21:32:36 -0700

    Add print (0-9) with putchar only script

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print (0...9)
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int  num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	};
+		putchar('\n');
+
+	return (0);
+}
