17645f7df7b587c8f122362fbdb13915a4235ae2
Tony_dev <tonyexplorer202@gmail.com>
2022-09-15 04:18:34 -0700

    Add test a postitive_or_negative() function script

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test fuction that prints if integer is positive or negative
+ * Return: Returns 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
