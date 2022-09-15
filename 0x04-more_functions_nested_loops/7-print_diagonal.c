aa21879e74295176029bce304b70a4f82c3962d1
Tony_dev <tonyexplorer202@gmail.com>
2022-09-15 13:00:45 -0700

    Add determines how many days are left in the year script

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+nclude "main.h"
+
+/**
+* print_diagonal - Print  diagonal lines with backslashes
+* @n: Amount of \ to be printed
+* Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i = 0, c;
+
+	while (i < n && n > 0)
+	{
+		c = 0;
+	while (c < i)
+	{
+		_putchar(' ');
+		c++;
+	}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
