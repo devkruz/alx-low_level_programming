41da61216b27e83ce534a3cdccac42fd89bedcde
Tony_dev <tonyexplorer202@gmail.com>
2022-09-21 08:54:08 -0700

    Fix betty code style

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,23 +2,24 @@
 
 /**
 * _strcat - Concatenates two strings
-* @dest: The array to be concatenated to 
+* @dest: The array to be concatenated to
 * @src: The array to concatenat
 * Return: The pointer to the concatenated array
 */
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+int i, j;
 
-	for(i = 0; dest[i] != '\0'; i++);
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-	for(j = 0; src[j] != '\0'; j++, i++)
-	{
-		dest[i] = src[j];
-	}
+for (j = 0; src[j] != '\0'; j++, i++)
+{
+dest[i] = src[j];
+}
 
-	dest[i] = '\0';
+dest[i] = '\0';
 
-	return (dest);
+return (dest);
 }
