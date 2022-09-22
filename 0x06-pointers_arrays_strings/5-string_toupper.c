f4f715cc38d6adb39ffab59f9a93835e7ae166eb
Tony_dev <tonyexplorer202@gmail.com>
2022-09-22 07:12:02 -0700

    Add changes all lowercase letters of a string to uppercase script

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* string_toupper - changes all lowercase letters of a string to uppercase
+* @a: The string to be uppercased
+* Return: Pointer to the amended array
+*/
+
+char *string_toupper(char *a)
+{
+	int length = strlen(a);
+	int i = 0;
+
+	while (i < length)
+	{
+		if (a[i] > 65 && a[i] < 90)
+		{
+		}
+		else if (a[i] > 97 && a[i] < 122)
+		{
+			a[i] -= 32;
+		}
+			i++;
+	}
+}
