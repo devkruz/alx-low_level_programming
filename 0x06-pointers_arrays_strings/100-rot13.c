ae3e83dc25f674573cf53dd1614224e94a5cd90c
Tony_dev <tonyexplorer202@gmail.com>
2022-09-22 21:00:51 -0700

    Add encodes a string using rot13 script

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* rot13 - Encodes a string using rot13
+* @a: The string to be endcoded
+* Return: Tht endcoded string
+*/
+
+char *rot13(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (((a[i] >= 65) && (a[i] < 78)) || ((a[i] >= 97) && (a[i] < 110)))
+		{
+			a[i] += 13;
+		}
+		else if (((a[i] >= 78) && (a[i] < 91)) || ((a[i] >= 110) && (a[i] < 123)))
+		{
+			a[i] -= 13;
+		}
+			i++;
+	}
+		return  (a);
+}
