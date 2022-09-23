cfa5d157eb11f3c6877f2328d7f17fa9a2df2477
Tony_dev <tonyexplorer202@gmail.com>
2022-09-23 07:35:12 -0700

    Fix encodes a string using rot13 script

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,4 @@
-
+#include "main.h"
 /**
 * rot13 - Encodes a string using rot13
 * @a: The string to be endcoded
@@ -7,19 +7,42 @@
 
 char *rot13(char *a)
 {
-	int i = 0;
+	int i_str = 0, i_let = 0;
 
-	while (a[i] != '\0')
+	char letters[] = {'A', 'B', 'C', 'D', 'E',
+			'F', 'G', 'H', 'I', 'J',
+			'K', 'L', 'M', 'N', 'O',
+			'P', "Q", 'R', 'S', 'T'
+			'U', 'V', 'W', 'X', 'Y',
+			'Z'', 'a', 'b', 'c', 'd',
+			'e', 'f', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q',
+			'r', 's', 't', 'u', 'v',
+			'w', 'x', 'y', 'z'};
+
+
+	char r_13[] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			'T', 'U', 'V', 'W', 'X', 'Y',
+			'Z', 'A', 'B', 'C', 'D', 'E',
+			'F', 'G', 'H', 'I', 'J', 'K',
+			'L', 'M', 'n', 'o', 'p', 'q',
+			'r', 's', 't', 'u', 'v', 'w',
+			'x', 'y', 'z', 'a', 'b', 'c',
+			'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm'};
+
+
+	while (str[i_str] != '\0')
 	{
-		if (((a[i] >= 65) && (a[i] < 78)) || ((a[i] >= 97) && (a[i] < 110)))
+		while (letters[i_let] != '\0')
 		{
-			a[i] += 13;
+			if (str[i_str] == letters[i_let])
+			{
+				str[i_str] = r_13[i_let];
+			}
+				i_let++;
 		}
-		else if (((a[i] >= 78) && (a[i] < 91)) || ((a[i] >= 110) && (a[i] < 123)))
-		{
-			a[i] -= 13;
-		}
-			i++;
+				i_str++;
 	}
-		return  (a);
+			return (str);
 }
