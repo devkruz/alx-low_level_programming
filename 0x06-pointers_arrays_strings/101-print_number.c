b8bd153690a09c061d0d238af047a966d628d3e2
Tony_dev <tonyexplorer202@gmail.com>
2022-09-22 21:08:04 -0700

    Add prints an integer script

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer
+* @n: The integar to be printed
+* return: void
+*/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+		_putchar(n % 10 + '0');
+}
