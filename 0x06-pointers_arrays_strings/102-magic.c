cc434b1abc15c746a0a5cc97a513e8b511cfe84e
Tony_dev <tonyexplorer202@gmail.com>
2022-09-23 08:22:06 -0700

    Revert "Solve 102-magic.c"
    This reverts commit 1cd813bacf3205ca60f9dc84a77629832db43d66.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
-* main - print 98
-* Return: void
-*/
 
 int main(void)
 {
@@ -20,7 +16,7 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-*(p + 5) = 98;
+printf("a[2] = %d\n", a[2]); return (0);
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
