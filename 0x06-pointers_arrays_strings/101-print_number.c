7f69ba0d81d410404191c049e9362d818bbe8fc7
Tony_dev <tonyexplorer202@gmail.com>
2022-09-23 08:07:52 -0700

    Fix 101-print_number.c compilation error

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-
+#include "main.h"
 /**
 * print_number - prints an integer
 * @n: The integar to be printed
@@ -10,12 +10,12 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		n = -n;
 	}
-	if (n / 10)
+	if ((n / 10) > 0)
 	{
 		print_number(n / 10);
 	}
 
-		_putchar(n % 10 + '0');
+		_putchar((n % 1)0 + '0');
 }
