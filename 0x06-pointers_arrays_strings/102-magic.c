1cd813bacf3205ca60f9dc84a77629832db43d66
Tony_dev <tonyexplorer202@gmail.com>
2022-09-23 08:16:34 -0700

    Solve 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+* main - print 98
+* Return: void
+*/
 
 int main(void)
 {
@@ -16,7 +20,7 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-printf("a[2] = %d\n", a[2]); return (0);
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
