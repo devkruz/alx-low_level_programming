89e386311eaac488c7618a653bfc8c07c8dee9a0
Tony_dev <tonyexplorer202@gmail.com>
2022-09-28 12:09:41 -0700

    Add return the length of a string script

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+
+
+/**
+* _strlen_recursion - returns the length of a string
+* @s: The string to return it length
+* Return: The length of the string
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
