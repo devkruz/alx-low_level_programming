983ec456c8d059b88d8471be77563d1cc0e902d2
Tony_dev <tonyexplorer202@gmail.com>
2022-09-30 07:13:41 -0700

    Add print my name function script

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Print the file name
+* @argc: The argument count
+* @argv: The array of argument
+*
+* Return: void
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
