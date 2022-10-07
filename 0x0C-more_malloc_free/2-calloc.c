692efffdbcd445cb3e1cb54258be6b9b8cfbe5b7
Tony_dev <tonyexplorer202@gmail.com>
2022-10-07 05:39:58 -0700

    Fix 2-calloc.c error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,13 +4,15 @@
 * _calloc - allocates memory for an array, using malloc
 * @nmemb: The amount of member in the array
 * @size: The size of each member
-* 
+*
 * Return: pointer to the new array
 */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *mem_ptr;
+	char *asign;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -19,6 +21,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (!mem_ptr)
 		return (NULL);
+
+	asign = mem_ptr;
+
+	for (i = 0; i < (nmemb * size); i++)
+		asign[i] = 0;
 
 	return (mem_ptr);
 }
