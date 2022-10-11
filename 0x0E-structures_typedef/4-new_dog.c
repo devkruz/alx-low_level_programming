b59d30a12e286b4c4808edc9bfaa2fea8e137f99
Tony_dev <tonyexplorer202@gmail.com>
2022-10-11 10:09:51 -0700

    Amend 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,21 +14,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t new_dog;
 	dog_t *new_dog_ptr;
-	char *name_ptr;
-	char *owner_ptr;
-	int i;
-
-	name_ptr = malloc(sizeof(name));
-	owner_ptr = malloc(sizeof(owner));
-
-	if (!name_ptr || !owner_ptr)
-		return (NULL);
-
-	for (i = 0; name[i]; i++)
-		name_ptr[i] = name[i];
-
-	for (i = 0; owner[i]; i++)
-		owner_ptr[i] = owner[i];
 
 	new_dog.name = name;
 	new_dog.age = age;
