8ca69cc9d94fc39de97b9cf68121367fe35e34ac
tonytec <tonyexplorer202@gmail.com>
2022-12-08 19:02:14 +0100

    Add header guard

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdio.h>
 
 /**
@@ -15,3 +17,7 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
