024c7e510e3ca15cf6674806e49afc95ce8c902d
tonytec <tonyexplorer202@gmail.com>
2023-01-06 17:00:15 +0100

    Add 3-hash_table_set.c file

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,63 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table
+ * @key: the node key
+ * @value: the node value
+ *
+ * Return: 1 if successfull else 0
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	unsigned long int size;
+	hash_node_t *tmp;
+
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+		return (0);
+
+	size = ht->size;
+	index = key_index((unsigned char *)key, size);
+
+	if (ht->array[index] == NULL)
+		ht->array[index] = create_hash_node(key, value);
+	else
+	{ /* collission occured */
+		tmp = ht->array[index]->next;
+		ht->array[index]->next = create_hash_node(key, value);
+		ht->array[index]->next->next = tmp;
+	}
+
+	return (1);
+}
+
+/**
+ * create_hash_node - create a new hash node
+ * @key: the node key
+ * @value: the node value
+ *
+ * Return: address to the new node
+*/
+
+hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new_hash_node;
+
+	new_hash_node = malloc(sizeof(hash_node_t));
+	if (new_hash_node == NULL)
+		exit(0);
+	new_hash_node->key = malloc(strlen(key) + 1);
+	if (new_hash_node->key == NULL)
+		exit(0);
+	new_hash_node->value = malloc(strlen(value) + 1);
+	if (new_hash_node->value == NULL)
+		exit(0);
+	new_hash_node->next = NULL;
+
+	strcpy(new_hash_node->key, key);
+	strcpy(new_hash_node->value, value);
+
+	return (new_hash_node);
+}
