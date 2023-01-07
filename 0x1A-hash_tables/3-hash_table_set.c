59d4fcbcd8325339f5844a52dd726cd3fd34abc7
tonytec <tonyexplorer202@gmail.com>
2023-01-07 15:43:22 +0100

    Add update logic for the node chain
    in 3-hash_table_set.c file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,19 +22,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, size);
 
 	if (ht->array[index] == NULL)
-		/* add new node */
+	{ /* add new node */
 		ht->array[index] = create_hash_node(key, value);
-	else if ((strcmp((ht->array[index]->key), key)) == 0)
-	{ /* update node */
-		free(ht->array[index]->value);
-		ht->array[index]->value = strdup(value);
+		return (1);
 	}
-	else
-	{ /* collission occured  */
-		tmp = ht->array[index];
-		ht->array[index] = create_hash_node(key, value);
-		ht->array[index]->next = tmp;
+
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if ((strcmp((tmp->key), key)) == 0)
+		{ /* update node */
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
 	}
+
+	/* collission occured  */
+	tmp = ht->array[index];
+	ht->array[index] = create_hash_node(key, value);
+	ht->array[index]->next = tmp;
 
 	return (1);
 }
