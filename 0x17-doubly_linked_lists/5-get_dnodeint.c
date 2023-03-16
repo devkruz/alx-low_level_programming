72e09369d790b92087983e9821618c25c898ab01
tonytec <tonyexplorer202@gmail.com>
2023-03-16 17:11:01 +0100

    Add 5-get_dnodeint.c file

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
+
+/**
+ * get_dnodeint_at_index - get the node at @index
+ * @head: the head of the node list
+ * @index: the index of the node to get
+ *
+ * Return: the node at @index
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	
+	dlistint_t *current = head;
+	unsigned int current_index = 0;
+
+	if (current == NULL)
+		return (NULL);
+
+	while (current != NULL)
+	{
+		if (current_index == index)
+			return (current);
+
+		current_index++;
+		current = current->next;
+	};
+
+	return (NULL);
 }
